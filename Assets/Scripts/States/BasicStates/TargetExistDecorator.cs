10ea0d2f001fca117ea031a06cfce525189e23a5
hanplay <tangent65@gmail.com>
2020-10-05 00:32:57 +0900

    Unity version 2020.1.7f Update

---
+++ Assets/Scripts/States/BasicStates/TargetExistDecorator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetExistDecorator : State
+{
+    private State state;
+    private Unit targetUnit;
+
+
+
+    public TargetExistDecorator(State state) : base(state.GetUnit())
+    {
+        this.state = state;
+    }
+    public override void Tick(float deltaTime)
+    {
+        state.Tick(deltaTime);
+    }
+
+    public override void End()
+    {
+        //basic state
+    }
+
+    public override bool IsEnded()
+    {
+        return state.IsEnded();
+    }
+
+    public override void SetNextState(State nextState)
+    {
+        state.SetNextState(nextState);
+    }
+
+    public override State GetNextState()
+    {
+        return state.GetNextState();
+    }
+}
