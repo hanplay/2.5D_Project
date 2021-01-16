3abfaf87eef26b4521950c7dd07e516ca9850bdd
hanplay <tangent65@gmail.com>
2021-01-16 21:39:50 +0900

    Bug that UI_HealthPointsModule is concealed by Units is fixed

---
+++ Assets/Scripts/DebugTool/DebugHealthPointsDamager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugHealthPointsDamager : MonoBehaviour
+{
+    private TrueDamageStrategy trueDamageStrategy = new TrueDamageStrategy();
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            List<Player> players = BattleSystem.Instance.GetPlayerWave().GetAllUnit();
+            for(int i = 0; i < players.Count; i++)
+            {
+                trueDamageStrategy.Do(players[i], 100);
+            }
+        }
+    }
+}
